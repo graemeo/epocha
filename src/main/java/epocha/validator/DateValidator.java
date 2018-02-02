34ef69828c78460d1abe9d8d4a1adebcad478aac
Graeme O <graeme.ong>
2018-02-02 23:33:48 +1100

    [GraemeO] Add month and day validation, and refactor year validation in Date Validator

--- src/main/java/epocha/validator/DateValidator.java
+++ src/main/java/epocha/validator/DateValidator.java
@@ -1,11 +1,27 @@
 package epocha.validator;
 
+import epocha.types.MonthType;
+
 public class DateValidator
 {
-    private static final int MAX_ALLOWABLE_YEAR = 2011;
-    private static final int MIN_ALLOWABLE_YEAR = 1899;
+    private static final int MAX_ALLOWABLE_YEAR = 2010;
+    private static final int MIN_ALLOWABLE_YEAR = 1900;
+    private static final int MAX_ALLOWABLE_MONTH = 12;
+    private static final int MIN_ALLOWABLE_MONTH = 1;
+    private static final int MIN_ALLOWABLE_DAY = 1;
 
     public static boolean isValidYear(int year) {
-        return year > MIN_ALLOWABLE_YEAR  && year < MAX_ALLOWABLE_YEAR;
+        return year >= MIN_ALLOWABLE_YEAR  && year <= MAX_ALLOWABLE_YEAR;
+    }
+
+    public static boolean isValidMonth(int month) {
+        return month >= MIN_ALLOWABLE_MONTH && month <= MAX_ALLOWABLE_MONTH;
+    }
+
+    public static boolean isValidDay(int day, int month, int year) {
+        boolean isLeapYear = year % 4 == 0;
+
+        return day >= MIN_ALLOWABLE_DAY 
+                  && day <= (isLeapYear && MonthType.FEBRUARY.getMonth() == month ? 29 : MonthType.getMonthInDays(month));
     }
 }
--- src/test/java/epocha/validator/DateValidatorTest.java
+++ src/test/java/epocha/validator/DateValidatorTest.java
@@ -36,4 +36,85 @@ public class DateValidatorTest
         // then
         assertTrue(DateValidator.isValidYear(date.getYear()));
     }
+
+    @Test
+    public void shouldReturnFalseWhenTheGivenMonthIsLaterThanTheMaximumAllowableMonth() {
+        // given
+        Date date = new Date(7, 15, 2001);
+
+        // when
+        // then
+        assertFalse(DateValidator.isValidMonth(date.getMonth()));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheGivenMonthIsLaterThanTheMinimumAllowableMonth() {
+        // given
+        Date date = new Date(5, 0, 2008);
+
+        // when
+        // then
+        assertFalse(DateValidator.isValidMonth(date.getMonth()));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTheGivenMonthIsWithinTheAllowableMonthRange() {
+        // given
+        Date date = new Date(10, 10, 2004);
+
+        // when
+        // then
+        assertTrue(DateValidator.isValidMonth(date.getMonth()));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheGivenDayIsEarlierThanTheMinimumAllowableDay() {
+        // given
+        Date date = new Date(0, 10, 2010);
+
+        // when
+        // then
+        assertFalse(DateValidator.isValidDay(date.getDay(), date.getMonth(), date.getYear()));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheGivenDayIsLaterThanTheMaximumAllowableDayBasedOnTheGivenMonth() {
+        // given
+        Date date = new Date(31, 4, 2010);
+
+        // when
+        // then
+        assertFalse(DateValidator.isValidDay(date.getDay(), date.getMonth(), date.getYear()));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTheGivenDayIsWithinTheAllowableDayBasedOnTheGivenMonth() {
+        // given
+        Date date = new Date(30, 4, 2010);
+
+        // when
+        // then
+        assertTrue(DateValidator.isValidDay(date.getDay(), date.getMonth(), date.getYear()));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheGivenDayIsLaterThanTheMaximumAllowableDayForFebruaryInNonLeapYear() {
+        // given
+        Date date = new Date(29, 2, 2009);
+
+        // when
+        // then
+        assertFalse(DateValidator.isValidDay(date.getDay(), date.getMonth(), date.getYear()));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTheGivenDayIsNoLaterThanTheMaximumAllowableDayForFebruaryInLeapYear() {
+        // given
+        Date date = new Date(29, 2, 2000);
+
+        // when
+        // then
+        assertTrue(DateValidator.isValidDay(date.getDay(), date.getMonth(), date.getYear()));
+    }
+
 }
